adf9c09dfbc7b0441eb518976c3de49510fad1ac
armiro <arman.hgh.1374@gmail.com>
2023-07-23 06:55:09 -0400

    update pylint score in the badge after each run

---
+++ utils/pylint_score_updater.py
@@ -0,0 +1,19 @@
+import subprocess
+import re
+
+# run Pylint and get the output
+pylint_output = subprocess.check_output(['pylint', '--exit-zero', '*.py'])
+
+# extract the Pylint score using regex
+score_match = re.search(r'Your code has been rated at (\d+.\d+)/10', pylint_output.decode())
+pylint_score = score_match.group(1) if score_match else "N/A"
+
+# update README.md with the Pylint badge and score
+with open("README.md", "r") as readme_file:
+    readme_content = readme_file.read()
+
+with open("README.md", "w") as readme_file:
+    updated_content = re.sub(pattern=r"!\[Pylint Score\].*",
+                             repl=f"![Pylint Score](https://img.shields.io/badge/PyLint-{pylint_score}/10-brightgreen)",
+                             string=readme_content)
+    readme_file.write(updated_content)
