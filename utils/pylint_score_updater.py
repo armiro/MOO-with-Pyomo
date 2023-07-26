c74312e2f66eea41ee62b68e18e8f19f62ad46b7
armiro <arman.hgh.1374@gmail.com>
2023-07-25 20:21:09 -0400

    fix running pylint for all files and add proper badge color

--- utils/pylint_score_updater.py
+++ utils/pylint_score_updater.py
@@ -1,19 +1,27 @@
+"""
+Run PyLint on all python files in the directory and add code quality score
+along with appropriate badge color to the readme file icons
+"""
 import subprocess
 import re
 
 # run Pylint and get the output
-pylint_output = subprocess.check_output(['pylint', '--exit-zero', '$(git ls-files "*.py")'])
+python_file_names = subprocess.check_output(['git', 'ls-files', '*.py']).decode().splitlines()
+pylint_output = subprocess.check_output(['pylint', '--exit-zero'] + python_file_names)
 
 # extract the Pylint score using regex
 score_match = re.search(r'Your code has been rated at (\d+.\d+)/10', pylint_output.decode())
-pylint_score = score_match.group(1) if score_match else "N/A"
+pylint_score = float(score_match.group(1)) if score_match else 0.0
+
+SCORE_COLOR = 'brightgreen' if pylint_score >= 8.0 else 'yellow' if pylint_score >= 6.0 else 'red'
 
 # update README.md with the Pylint badge and score
-with open("README.md", "r") as readme_file:
+with open("README.md", mode="r") as readme_file:
     readme_content = readme_file.read()
 
-with open("README.md", "w") as readme_file:
-    updated_content = re.sub(pattern=r"!\[Pylint Score\].*",
-                             repl=f"![Pylint Score](https://img.shields.io/badge/PyLint-{pylint_score}/10-brightgreen)",
+with open("README.md", mode="w") as readme_file:
+    updated_content = re.sub(pattern=r"!\[Pylint\].*",
+                             repl=f"![Pylint](https://img.shields.io/badge/PyLint-"
+                                  f"{pylint_score}/10-{SCORE_COLOR})",
                              string=readme_content)
     readme_file.write(updated_content)
