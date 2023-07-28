4e7a56407937ec3085ff58f3069cee0aa25669ee
armiro <arman.hgh.1374@gmail.com>
2023-07-28 19:16:10 -0400

    fix epsilon range and change epsilon value generation method

--- main.py
+++ main.py
@@ -5,17 +5,27 @@ Developed by Arman H. (https://github.com/armiro)
 import random
 import pyomo.environ as pyo
 import matplotlib.pyplot as plt
+import numpy as np
 
 import output_functions as out_fns
 import input_variable_values as in_vars
 
 
-def plot_pareto_front(of1_values, of1_name, of2_values, of2_name):
-    """plot pareto-front plot as a scatter plot with num_optimal_solutions points"""
+def plot_pareto_fronts(**objective_functions):
+    """
+    plot pareto-front plot as a scatter plot with num_optimal_solutions points
+    :param objective_functions: kwargs containing a number of obj func names and values
+    :return: None
+    """
+    names = list()
+    of_values = list()
+    for name, values in objective_functions.values():
+        names.append(name)
+        of_values.append(values)
     # fig = plt.figure(num=1)
-    plt.scatter(of1_values, of2_values)
-    plt.xlabel(of1_name)
-    plt.ylabel(of2_name)
+    plt.scatter(of_values[1], of_values[2])
+    plt.xlabel(names[1])
+    plt.ylabel(names[2])
     plt.title("Pareto-Front Plot")
     plt.grid(True)
     plt.show()
@@ -24,13 +34,14 @@ def plot_pareto_front(of1_values, of1_name, of2_values, of2_name):
 # create a concrete Pyomo model
 model = pyo.ConcreteModel(name='Pyomo Model')
 
-# define the independent variables
-# model.h_conc = pyo.Var(bounds=in_vars.hardness_conc_bounds, domain=pyo.NonNegativeReals, initialize=250.0)
-# model.s_conc = pyo.Var(bounds=in_vars.silica_conc_bounds, domain=pyo.NonNegativeReals, initialize=5.0)
-# model.ph = pyo.Var(bounds=in_vars.initial_ph_bounds, domain=pyo.NonNegativeReals, initialize=3.0)
-# model.time = pyo.Var(bounds=in_vars.contact_time_bounds, domain=pyo.NonNegativeReals, initialize=30.0)
-# model.current = pyo.Var(bounds=in_vars.current_density_bounds, domain=pyo.NonNegativeReals, initialize=2.2)
+# define the independent variables (raw values)
+# model.h_conc = pyo.Var(bounds=in_vars.hardness_conc_bounds, domain=pyo.NonNegativeReals)
+# model.s_conc = pyo.Var(bounds=in_vars.silica_conc_bounds, domain=pyo.NonNegativeReals)
+# model.ph = pyo.Var(bounds=in_vars.initial_ph_bounds, domain=pyo.NonNegativeReals)
+# model.time = pyo.Var(bounds=in_vars.contact_time_bounds, domain=pyo.NonNegativeReals)
+# model.current = pyo.Var(bounds=in_vars.current_density_bounds, domain=pyo.NonNegativeReals)
 
+# define the independent variables (coded values)
 model.h_conc = pyo.Var(bounds=in_vars.coded_bounds, domain=pyo.NonNegativeReals)
 model.s_conc = pyo.Var(bounds=in_vars.coded_bounds, domain=pyo.NonNegativeReals)
 model.ph = pyo.Var(bounds=in_vars.coded_bounds, domain=pyo.NonNegativeReals)
@@ -53,15 +64,20 @@ opt = pyo.SolverFactory('ipopt', executable='./optimizer/ipopt.exe')
 # define the number of optimal solutions
 NUM_OPTIMAL_SOLUTIONS = 30
 
-# define epsilon value ranges for each objective function (experimentally)
-hre_epsilon_range = (2, 10)
-sre_epsilon_range = (0.2, 1)
-oc_epsilon_range = (0.5, 2)
+# define epsilon range for each objective function (experimentally: min_of <= e <= max_of)
+e_hre_range = (out_fns.hardness_removal_efficiency.min, out_fns.hardness_removal_efficiency.max)
+e_sre_range = (out_fns.silica_removal_efficiency.min, out_fns.silica_removal_efficiency.max)
+e_oc_range = (out_fns.operating_cost.min, out_fns.operating_cost.max)
 
 # generate random epsilon values for each objective function
-e_hre_vals = [round(random.uniform(*hre_epsilon_range), 2) for _ in range(NUM_OPTIMAL_SOLUTIONS)]
-e_sre_vals = [round(random.uniform(*sre_epsilon_range), 2) for _ in range(NUM_OPTIMAL_SOLUTIONS)]
-e_oc_vals = [round(random.uniform(*oc_epsilon_range), 2) for _ in range(NUM_OPTIMAL_SOLUTIONS)]
+# e_hre_vals = [round(random.uniform(*e_hre_range), 2) for _ in range(NUM_OPTIMAL_SOLUTIONS)]
+# e_sre_vals = [round(random.uniform(*e_sre_range), 2) for _ in range(NUM_OPTIMAL_SOLUTIONS)]
+# e_oc_vals = [round(random.uniform(*e_oc_range), 2) for _ in range(NUM_OPTIMAL_SOLUTIONS)]
+
+# generate epsilon values with similar distance for each objective function
+e_hre_vals = np.arange(*e_hre_range, (e_hre_range[1]-e_hre_range[0])/NUM_OPTIMAL_SOLUTIONS)
+e_sre_vals = np.arange(*e_sre_range, (e_sre_range[1]-e_sre_range[0])/NUM_OPTIMAL_SOLUTIONS)
+e_oc_vals = np.arange(*e_oc_range, (e_oc_range[1]-e_oc_range[0])/NUM_OPTIMAL_SOLUTIONS)
 
 # create a dictionary to store results
 all_results = {}
@@ -73,10 +89,10 @@ for e_hre, e_sre, e_oc in zip(e_hre_vals, e_sre_vals, e_oc_vals):
     model.e_constraint_sre = pyo.Constraint(expr=model.sre <= e_sre)
     model.e_constraint_oc = pyo.Constraint(expr=model.oc <= e_oc)
 
-    # Set the active objective to be maximized/minimized
-    model.o_hre.activate()
+    # set the objective to be optimized (ONLY ONE can be set activated simultaneously)
+    model.o_hre.deactivate()
     model.o_sre.deactivate()
-    model.o_oc.deactivate()
+    model.o_oc.activate()
 
     # solve the model using optimizer
     results = opt.solve(model, tee=True)
@@ -98,16 +114,21 @@ for e_hre, e_sre, e_oc in zip(e_hre_vals, e_sre_vals, e_oc_vals):
     model.del_component(model.e_constraint_sre)
     model.del_component(model.e_constraint_oc)
 
+# print the results
 for e, result in all_results.items():
     print(f"Epsilon: {e}")
     for key, value in result.items():
         print(f"{key}: {value}")
-    print("=" * 30)
+    print("===" * 10)
 
-# Extract the objective values from all_results
-objective1_values = [result['Objective 1 (hre)'] for result in all_results.values()]
-objective2_values = [result['Objective 2 (sre)'] for result in all_results.values()]
+# extract the objective values from all_results
+obj1_values = [result['Objective 1 (hre)'] for result in all_results.values()]
+obj2_values = [result['Objective 2 (sre)'] for result in all_results.values()]
+obj3_values = [result['Objective 3 (oc)'] for result in all_results.values()]
 
-# Plot the Pareto front using a scatter plot
-plot_pareto_front(of1_values=objective1_values, of1_name=model.hre.getname(),
-                  of2_values=objective2_values, of2_name=model.sre.getname())
+# plot all pareto fronts using a scatter plot
+plot_pareto_fronts(of1=(model.hre.getname(), obj1_values),
+                   of2=(model.sre.getname(), obj2_values),
+                   of3=(model.oc.getname(), obj3_values))
+
+
